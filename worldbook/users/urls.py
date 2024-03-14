6207a350bb4e1a6bdbcdeb221718f14ad49baf16
RaiTak <kairat_raitak@mail.ru>
2024-03-14 22:40:32 +0600

    0.09 Create PasswordChange

--- worldbook/users/urls.py
+++ worldbook/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeDoneView
 from django.urls import path
 from users import views
 
@@ -9,4 +9,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='main:home'), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
+
+    path('password_change/', views.UserPasswordChange.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(
+        template_name='users/password_change_done.html'), name='password_change_done'),
+
 ]
